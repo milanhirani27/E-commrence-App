c11c7b77e71abb6a7bb502433684690a34105e6e
milanhirani27 <milanhirani2712@gmail.com>
2024-10-19 18:16:52 +0530

    fix issue

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,7 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import ProductListingScreen from '../Screens/ProductListingScreen/ProductListingScreen';
 import CartScreen from '../Screens/CartScreen/CartScreen';
-import CheckOutScreen from '../Screens/CheckOutScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,9 +10,8 @@ const AppNavigation = () => {
     <Stack.Navigator
       screenOptions={{ headerShown: false   }}
     >
-      {/* <Stack.Screen name="productListing" component={ProductListingScreen} />
-      <Stack.Screen name='cartScreen' component={CartScreen} /> */}
-      <Stack.Screen name='checkout' component={CheckOutScreen} />
+      <Stack.Screen name="productListing" component={ProductListingScreen} />
+      <Stack.Screen name='cartScreen' component={CartScreen} />
     </Stack.Navigator>
   );
 }
